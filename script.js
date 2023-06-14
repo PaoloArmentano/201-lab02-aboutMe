91dd9adfc4ba063227ac39fd4acb0eedc952b51a
PaoloArmentano <armentano.paolo@gmail.com>
2023-06-14 21:31:09 +0100

    changed the JS

--- script.js
+++ script.js
@@ -1,78 +1,104 @@
-// "use strict";
+"use strict";
 
-// let userName = prompt("Hi, please enter your name if you wanna play a game");
-// console.log(userName);
+let userName = prompt("Hi, please enter your name if you wanna play a game");
+console.log(userName);
 
-// alert(`Hi ${userName}, wanna take a quiz about me?`)
+alert(`Hi ${userName}, wanna take a quiz about me?`)
 
-// let fullName = "Paolo Armentano";
+let fullName = "Paolo Armentano";
 
-// let myFullName = prompt("What's my full name? (first and last name)");
+let myFullName = prompt("What's my full name? (first and last name)");
 
-// if(myFullName.toLowerCase() === fullName.toLowerCase()){
-//     alert("You are correct");
-// } else{
-//     alert("Come on bro that was an easy one and you still got it wrong");
-// }
-
-
-
-// let myFavouritePet = "Cats";
-
-// let myPets = prompt("What's my favourite pet? (plural noun)");
-
-// if (myPets.toLowerCase().includes("cat")) {
-//     alert("You are correct!");
-//   } else {
-//     alert(`No, ${myPets} are nice, but I prefer cats.`);
-//   }
+if(myFullName.toLowerCase() === fullName.toLowerCase()){
+    alert("You are correct");
+} else{
+    alert("Come on bro that was an easy one and you still got it wrong");
+}
 
 
 
-// let myMainInstrument = "Guitar";
+let myFavouritePet = "Cats";
 
-// let guitarAnswer = prompt("What's the main instrument I play?");
+let myPets = prompt("What's my favourite pet? (plural noun)");
 
-// if(myMainInstrument.toLowerCase() === guitarAnswer.toLowerCase()){
-//     alert("You are correct");
-// } else{
-//     alert("Mate you can literally see the guitars behind me, how did you get that wrong.");
-// }
-
-
-
-// let placeOfBirth = "Italy";
-
-// let coutryBirth = prompt("Which country was I born in?");
-
-// if(placeOfBirth.toLowerCase() === coutryBirth.toLowerCase()){
-//     alert("You are correct");
-// } else{
-//     alert("How did you get that wrong? Look at my name. Anyway, I'm italian.");
-// }
+if (myPets.toLowerCase().includes("cat")) {
+    alert("You are correct!");
+  } else {
+    alert(`No, ${myPets} are nice, but I prefer cats.`);
+  }
 
 
 
-// let age = 28;
-// let attempts = 4;
+let myMainInstrument = "Guitar";
 
-// while (attempts > 0) {
-//   let ageGuess = parseInt(prompt("How old am I? (You have " + attempts + " guesses)"));
+let guitarAnswer = prompt("What's the main instrument I play?");
 
-//   if (ageGuess === age) {
-//     alert("You are correct!");
-//     break; // Exit the loop if the guess is correct
-//   } else if (ageGuess >= 29) {
-//     alert("Wrong! Do I really look that old? That's borderline offensive. Try again.");
-//   } else if (ageGuess <= 27) {
-//     alert("Wrong! Do I really look that young? Try again.");
-//   }
+if(myMainInstrument.toLowerCase() === guitarAnswer.toLowerCase()){
+    alert("You are correct!");
+} else{
+    alert("Mate you can literally see the guitars behind me, how did you get that wrong.");
+}
 
-//   attempts--;
-// }
 
-// // If all attempts are used and the age is not guessed correctly
-// if (attempts === 0) {
-//   alert("Sorry, you've used all your attempts. My actual age is " + age + ".");
-// }
+
+let placeOfBirth = "Italy";
+
+let coutryBirth = prompt("Which country was I born in?");
+
+if(placeOfBirth.toLowerCase() === coutryBirth.toLowerCase()){
+    alert("You are correct");
+} else{
+    alert("How did you get that wrong? Look at my name. Anyway, I'm italian.");
+}
+
+
+
+let age = 28;
+let attempts = 4;
+
+while (attempts > 0) {
+  let ageGuess = parseInt(prompt("How old am I? (You have " + attempts + " guesses)"));
+
+  if (ageGuess === age) {
+    alert("You are correct!");
+    break; // Exit the loop if the guess is correct
+  } else if (ageGuess >= 29) {
+    alert("Wrong! Do I really look that old? That's borderline offensive. Try again.");
+  } else if (ageGuess <= 27) {
+    alert("Wrong! Do I really look that young? Try again.");
+  }
+
+  attempts--;
+}
+
+// If all attempts are used and the age is not guessed correctly
+if (attempts === 0) {
+  alert("Sorry, you've used all your attempts. My actual age is " + age + ".");
+}
+
+
+
+let myFavouriteSport = ["Football","MMA","Basketball","Boxing","Skiing","Rugby"];
+let attempts = 6;
+
+while (attempts > 0) {
+  let sportGuess = prompt("What's my favourite sport? (You have " + attempts + " guesses)");
+
+  if (sportGuess === "MMA".toLowerCase()) {
+    alert("You are correct!");
+    break; // Exit the loop if the guess is correct
+  } else {
+    alert("Wrong! Try again.");
+  }
+
+  attempts--;
+}
+
+// If all attempts are used and the age is not guessed correctly
+if (attempts === 0) {
+  alert("Sorry, you've used all your attempts. My actual age favourite sport is MMA.");
+}
+
+
+
 
